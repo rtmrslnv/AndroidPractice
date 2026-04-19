3dbb3657f9bf296c6841a6517b5077683b41e911
rtmrslnv <artemaraslanov4@gmail.com>
2026-04-19 10:02:17 +0500

    practice 7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
 
     defaultConfig {
         applicationId = "ru.rtmrslnv.androidpractice"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
